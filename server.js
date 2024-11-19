175a3540f3a2644648a0b25e14b609b4bcf3bd74
Dilum-IR <rdinduwara19158@gmail.com>
2024-11-19 10:06:06 +0530

    change the port.

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 const app = require("./app");
 
-// const port = process.env.PORT || 8000;
-const port = 8000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
